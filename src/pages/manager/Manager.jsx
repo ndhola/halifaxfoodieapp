92df8119d3df0e2c19f8472b45307a57cb030ecb
Nikunj Dhola <nk309459@dal.ca>
2021-07-26 12:44:33 -0300

    Final Change

--- src/pages/manager/Manager.jsx
+++ src/pages/manager/Manager.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FoodItems from "./foodItems/FoodItems";
+import Sentiment from "./sentiment/Sentiment";
 
 const Manager = ({ user }) => {
   return (
     <div>
       <FoodItems user={user} />
+      <Sentiment />
     </div>
   );
 };
